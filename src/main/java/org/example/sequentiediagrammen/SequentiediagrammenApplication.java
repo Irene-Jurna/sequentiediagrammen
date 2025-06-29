c9fb8f862e8618d5339ac7b8910b11496ea86387
Irene <irene.jurna@gmail.com>
2025-06-29 20:00:44 +0200

    Initial commit

---
+++ src/main/java/org/example/sequentiediagrammen/SequentiediagrammenApplication.java
@@ -0,0 +1,13 @@
+package org.example.sequentiediagrammen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequentiediagrammenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SequentiediagrammenApplication.class, args);
+    }
+
+}
